23af397662379d3e28f933e517474d9dc1eab484
Mahdi <me@mahdif.com>
2015-12-26 21:49:08 +0100

    editing the comments

--- js/youtube-cover-video.js
+++ js/youtube-cover-video.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // 2. This code loads the IFrame Player API code asynchronously.
+  // This code loads the IFrame Player API code asynchronously.
   var tag = document.createElement('script');
 
   tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  // 3. This function creates an <iframe> (and YouTube player)
-  //    after the API code downloads.
+  //  This function creates an <iframe> (and YouTube player)
+  //  after the API code downloads.
   var player;
   onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady() {
     player = new YT.Player('YTplayer', {
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // 4. The API will call this function when the video player is ready.
+  // The API will call this function when the video player is ready.
   function onPlayerReady(event) {
     event.target.playVideo();
     player.mute();
